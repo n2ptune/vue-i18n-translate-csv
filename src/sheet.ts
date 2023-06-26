96d785931044cf9ca105221fd1cd6bbbb678f0da
n2ptune <n2ptune@outlook.com>
2023-06-26 18:17:18 +0900

    test

--- src/sheet.ts
+++ src/sheet.ts
@@ -1,3 +1,8 @@
+import { google } from 'googleapis'
+import { client_email, private_key } from '../credentials.json'
+
 export function runInSheet(full: string, words: string[], fn: string[]) {
-  console.log(full, words, fn)
+  const authorize = new google.auth.JWT(client_email, null, private_key, [
+    'https://www.googleapis.com/auth/spreadsheets',
+  ]);
 }
